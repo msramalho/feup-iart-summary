70255b04b2cbfe078f82e42273b553f97f582f34
msramalho <up201403027@fe.up.pt>
2018-04-30 11:48:46 +0100

    interogative sentences with

---
+++ praticas/8/8.2.pl
@@ -0,0 +1,129 @@
+clear:-write('\33\[2J').
+% 8
+
+% 8.1
+% N de número
+% G de género
+% S de sujeito
+% A de ação
+% S de sujeito
+% Ob de objeto
+
+frase(A, S, Ob) --> sn(N, S), sv(N, A, Ob, S).
+sn(N, S) --> determinante(N-G), nome(N-G, S).
+sn(N, S) --> nome(N-_, S).
+sv(N, gostar, Ob, S) --> verbo(N, gostar, S), {!}, proposicao(N1-G1), nome(N1-G1, Ob). % Nota 1
+sv(N, A, Ob, S) --> verbo(N, A, S), sn(_, Ob).
+
+concorda_frase(A, S, Ob):-
+	P =.. [A, S, Ob], % link 1, Nota 2
+	(P, !, write(concordo); write(discordo)).
+
+% Notas:
+% 1 - as chavetas são código prolog não DCG, neste caso é um cut para que o joão gosta morangos não seja válidas
+% 2 - prolog é de primeira ordem e não de segunda, não há relações entre objetos que sejam quantificadores, é preciso operador extra-lógico univ
+
+% Links:
+% 1- http://www.swi-prolog.org/pldoc/doc_for?object=(%3D..)/2
+
+% Exemplos:
+% frase(A, S, Ob, [o,rui,joga,futebol], []). yes
+% frase(A, S, Ob, [os,rui,joga,futebol], []). no
+% frase(A, S, Ob, [o,rui,joga,futebol], []), concorda_frase(A, S, Ob). yes
+% frase(A, S, Ob, [o, elefante, joga, futebol], []).
+
+%%%
+% gramática
+%
+
+determinante(s-m) --> [o].
+determinante(p-m) --> [os].
+determinante(s-f) --> [a].
+
+preposicao(_) --> [de].
+preposicao(s-f) --> [da].
+
+nome(p-m, rapaz) --> [rapazes].
+nome(s-m, rapaz) --> [rapaz].
+nome(s-m, rui) --> [rui].
+nome(s-m, luis) --> [luis].
+nome(s-f, rita) --> [rita].
+nome(s-f, ana) --> [ana].
+nome(s-f, maria) --> [maria].
+nome(s-m, elefante) --> [elefante].
+nome(p-m, cao) --> [caes].
+nome(p-m, gato) --> [gatos].
+nome(s-m, cao) --> [cao].
+nome(s-m, gato) --> [gato].
+nome(s-m, futebol) --> [futebol].
+nome(p-m, morango) --> [morangos].
+nome(p-m, amendoim) --> [amendoins].
+nome(p-m, bolacha) --> [bolachas].
+nome(p-m, humano) --> [humanos].
+nome(p-f, pessoa) --> [pessoas].
+
+verbo(s, jogar, S) --> [joga], {humano(S)}. % nota 1
+verbo(p, jogar, S) --> [jogam], {humano(S)}. % nota 1
+verbo(s, gostar, _) --> [gosta].
+verbo(p, gostar, _) --> [gostam].
+verbo(s, comer, _) --> [come].
+verbo(p, comer, _) --> [comem].
+verbo(p, ser, _) --> [sao].
+
+%%%
+% base de dados
+%
+
+humano(rapaz).
+humano(rui).
+humano(maria).
+humano(rita).
+humano(ana).
+humano(luis).
+humano(humano).
+humano([]).
+humano([H|T]) :- humano(H), humano(T).
+
+jogar(rapaz, futebol).
+jogar(rui, futebol).
+jogar(pokemon, futebol).
+
+gostar(luis, morango).
+gostar(rita, morango).
+gostar(ana, morango).
+gostar(rui, maria).
+gostar(cao, bolacha).
+gostar(gato, bolacha).
+
+comer(elefante, amendoim).
+
+ser(rui, rapaz).
+ser(X, humano) :- humano(X).
+
+
+% 8.2
+% extensão feita para 8.2 -> inclui sintagma verbal
+
+%%%
+% gramática
+%
+
+pron_inter(_-_) --> [quem].
+pron_inter(p-_) --> [quais].
+pron_inter(p-m) --> [quantos].
+pron_inter(p-f) --> [quantas].
+
+pronome(_) --> [que].
+
+
+% DCG
+
+frase_i --> si(N), sv(N, _, _, _).
+si(N) --> pron_inter(N-G), sni(N-G).
+si(N) --> pron_inter(N-G).
+sni(N-G) --> determinante(N-G), nome(N-G, _), [que].
+sni(N-G) --> nome(N-G, _).
+
+
+
+
